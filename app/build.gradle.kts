fd435ebe8eff51a0957294477f213ff93cb380de
Expliyh <expliyh@outlook.com>
2025-12-26 17:16:51 +0800

    Update versioning and Gradle configuration for project
    Signed-off-by: Expliyh <expliyh@outlook.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -28,8 +28,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
